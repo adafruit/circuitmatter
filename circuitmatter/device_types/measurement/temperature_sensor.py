07186824ff9cc6b306d374329a4985c39870fab0
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-10-17 15:46:32 +0200

    Create temperature_sensor.py

---
+++ circuitmatter/device_types/measurement/temperature_sensor.py
@@ -0,0 +1,19 @@
+from circuitmatter.clusters.general.identify import Identify
+from circuitmatter.clusters.general.temperature_measurement import TemperatureMeasurement
+
+from .. import simple_device
+
+
+class TemperatureSensor(simple_device.SimpleDevice):
+    DEVICE_TYPE_ID = 0x0302
+    REVISION = 2
+
+    def __init__(self, name):
+        super().__init__(name)
+
+        self._identify = Identify()
+        self.servers.append(self._identify)
+
+        self._temp = TemperatureMeasurement()
+        self.servers.append(self._temp)
+  
