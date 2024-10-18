5bcf5ff40d2b834a42f7ecd4a020e79a2555c422
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-10-18 07:39:45 +0200

    Set MeasuredValue attributes nullable
    Co-authored-by: Scott Shawcroft <scott@tannewt.org>

--- circuitmatter/clusters/general/temperature_measurement.py
+++ circuitmatter/clusters/general/temperature_measurement.py
@@ -5,10 +5,10 @@ class TemperatureMeasurement(data_model.Cluster):
     CLUSTER_ID = 0x0402
     REVISION = 4
 
-    MeasuredValue = data_model.NumberAttribute(0x0000, signed=True, bits=16, default=0)
+    MeasuredValue = data_model.NumberAttribute(0x0000, signed=True, bits=16, default=0, X_nullable=True, P_reportable=True)
     MinMeasuredValue = data_model.NumberAttribute(
-        0x0001, signed=True, bits=16, default=-5000
+        0x0001, signed=True, bits=16, default=-5000, X_nullable=True
     )
     MaxMeasuredValue = data_model.NumberAttribute(
-        0x0002, signed=True, bits=16, default=15000
+        0x0002, signed=True, bits=16, default=15000, X_nullable=True
     )
