996882770d0e9ae69f5266d395a4e83980dc7d71
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-10-18 07:38:51 +0200

    A revision to Temp cluster
    Co-authored-by: Scott Shawcroft <scott@tannewt.org>

--- circuitmatter/clusters/general/temperature_measurement.py
+++ circuitmatter/clusters/general/temperature_measurement.py
@@ -3,6 +3,7 @@ from circuitmatter import data_model
 
 class TemperatureMeasurement(data_model.Cluster):
     CLUSTER_ID = 0x0402
+    REVISION = 4
 
     MeasuredValue = data_model.NumberAttribute(0x0000, signed=True, bits=16, default=0)
     MinMeasuredValue = data_model.NumberAttribute(
