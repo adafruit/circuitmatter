1a8e90ba8fe6ae42a359b4e8ed616534c6530c25
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-10-19 11:23:40 +0200

    Update temperature_measurement.py

--- circuitmatter/clusters/measurement/temperature_measurement.py
+++ circuitmatter/clusters/measurement/temperature_measurement.py
@@ -5,7 +5,9 @@ class TemperatureMeasurement(data_model.Cluster):
     CLUSTER_ID = 0x0402
     REVISION = 4
 
-    MeasuredValue = data_model.NumberAttribute(0x0000, signed=True, bits=16, default=0, X_nullable=True, P_reportable=True)
+    MeasuredValue = data_model.NumberAttribute(
+        0x0000, signed=True, bits=16, default=0, X_nullable=True, P_reportable=True
+    )
     MinMeasuredValue = data_model.NumberAttribute(
         0x0001, signed=True, bits=16, default=-5000, X_nullable=True
     )
