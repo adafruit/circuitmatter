5bea854fbb18fc879cf27a275c0552fafa1c0a2c
Scott Shawcroft <scott@tannewt.org>
2024-10-23 15:06:48 -0700

    Bump to 0.3.0

--- circuitmatter/__init__.py
+++ circuitmatter/__init__.py
@@ -15,7 +15,7 @@ from . import session
 from .subscription import Subscription
 from .device_types.utility.root_node import RootNode
 
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 
 
 class CircuitMatter:
